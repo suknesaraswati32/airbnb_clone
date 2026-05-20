1265f13331f0d91672df1d4f143a8ca4e04ee267
sara <suknesaraswati8@gmail.com>
2026-05-20 23:08:34 +0530

    adding a new project

---
+++ app.js
@@ -0,0 +1,38 @@
+const express=require("express")
+const app=express()
+const mongoose=require("mongoose")
+const Listing=require("./models/listing.js")
+const path=require("path")
+
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust"
+main().then(() => console.log("Connected to MongoDB"))
+.catch(err => console.log(err));
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"))
+async function main() {
+  await mongoose.connect(MONGO_URL);
+
+}
+app.get("/",(req,res)=>{
+  res.send("Hello World")
+})
+// app.get("/listings",async(req,res)=>{
+//   let sampleListing=new Listing({
+//     title:"new palace",
+//     description:"a beautiful palace in the heart of the city",
+//     price:500000,
+//     location:"Kolhapur,Maharashtra",
+//     country:"India"
+// //   })
+//   await sampleListing.save()
+//   console.log("listing was saved")
+//   res.send("Listing created")
+// })
+
+app.get("/listings/all",async(req,res)=>{
+  let listings=await Listing.find({})
+  res.render("/listings/index.ejs", { listings })
+})
+app.listen(8080,()=>{
+  console.log("server is running on port 8080")
+})
---
+++ models/listing.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose")
+const Schema= mongoose.Schema
+
+const listingSchema=new Schema({
+  title:{
+    type: String,
+    required:true
+  },
+  description:String,
+    image: {
+    filename: {
+      type: String,
+      default: "listingimage"
+    },
+
+    url: {
+      type: String,
+      default:
+        "https://images.rawpixel.com/image_png_social_landscape/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsX29mZmljZV8xOV9tb2Rlcm5faG91c2VfaXNvbGF0ZWRfb25fd2hpdGVfYmFja2dyb3VuZF9lOTdkNjgxZC1mN2U1LTRmMjUtYTllYi1mNjJlOWJlN2U3ZDIucG5n.png",
+         set:(v)=> v===""? "https://images.rawpixel.com/image_png_social_landscape/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsX29mZmljZV8xOV9tb2Rlcm5faG91c2VfaXNvbGF0ZWRfb25fd2hpdGVfYmFja2dyb3VuZF9lOTdkNjgxZC1mN2U1LTRmMjUtYTllYi1mNjJlOWJlN2U3ZDIucG5n.png":v,
+    }
+  },
+  price:Number,
+  location:String,
+  country:String
+})
+
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
