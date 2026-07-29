4bd772cfe893f201473b09cebc81d05c9c321436
sara <suknesaraswati8@gmail.com>
2026-07-29 18:16:35 +0530

    create login and signup pages

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const mongoose=require("mongoose")
 const path=require("path")
 const methodOverride = require('method-override')
 const ejsMate=require("ejs-mate")
-const listings=require("./routes/listing.js")
-const reviews=require("./routes/reviews.js")
+const listingRouter=require("./routes/listing.js")
+const reviewRouter=require("./routes/reviews.js")
+const userRouter=require("./routes/user.js")
 const { error } = require("console")
 const ExpressError=require("./utils/ExpressError.js")
 const Listing=require("./models/listing.js")
 const session=require("express-session")
 const flash=require("connect-flash")
+const passport=require("passport")
+const LocalStrategy = require("passport-local");
+const User=require("./models/user.js")
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust"
 main()
 .then(() =>
@@ -34,17 +38,33 @@ const sessionoptions={
 }
 app.use(session(sessionoptions))
 app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.use((req,res,next)=>{
 res.locals.success=req.flash("success")
 res.locals.error=req.flash("error")
 next()
 })
-app.use("/listings",listings)
-app.use("/listings/:id/reviews",reviews)
+app.use("/listings",listingRouter)
+app.use("/listings/:id/reviews",reviewRouter)
+app.use("/",userRouter)
 app.engine("ejs",ejsMate)
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
+
+// app.get("/demouser",async(req,res)=>{
+//   let fakeUser=new User({
+//     email:"stuldent@gmail.com",
+//     username:"delta-student"
+//   })
+//  let registereduser= await User.register(fakeUser,"helloworld")
+//  res.send(registereduser)
+// })
+
 
 // app.get("/listings",async(req,res)=>{
 //   let sampleListing=new Listing({
